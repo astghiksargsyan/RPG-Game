fa384fa5bf341b5f20781cfa253cdccf1065d7d1
Astghik <astx.kras@gmail.com>
2026-07-06 13:42:50 +0400

    Added menu

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 from models.game_manager import GameManager
 from models.character import Character
+from models.archer import Archer
+from models.warrior import Warrior
+from models.mage import Mage
+from models.monster import Monster
+from models.enemy import Enemy
+heroes = [Mage(), Warrior(), Archer()]
+villains = [Enemy(), Monster()]
 def main():
-    GameManager.start()
-    print(Character.total_characters_count)
+    while True:
+        print("1. Start Game")
+        print("2. Show Characters")
+        print("3. Exit")
+        choice = input("Choose option: ")
+        if choice == "1":
+            GameManager.start()
+        elif choice == "2":
+            print("You can choose from heroes:")
+            for hero in heroes:
+                print(hero.name)
+            print("Or you can choose from villains:")
+            for villain in villains:
+                print(villain.name)
+        elif choice == "3":
+            break
+        else:
+            print("Invalid option")
 main()
